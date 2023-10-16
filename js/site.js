f503fd7116a8fb839b1294663abf667d5b7e5c80
Thomas Bell <thomasjbell065@gmail.com>
2023-10-16 16:44:03 -0400

    completed project, need to style

--- js/site.js
+++ js/site.js
@@ -1,17 +1,45 @@
-function displayMessage() {
-    // get the text out of our <input> and assign it to a variable
-    let msg = document.getElementById('message').value;
+// get the user's message to reverse
+function getValues() {
 
-    // alert('Hello Guardian Cohort!')
-    Swal.fire(
-        {
-            icon: 'success',
+    // get the text input from the page
+    let message = document.getElementById('userInput').value;
+
+    // validate the input: make sure the input is not empty
+    if (message.length == 0) {
+        Swal.fire ({
+            icon: 'error',
             backdrop: false,
-            title: '<strong>APP NAME</strong>',
-            text: msg, // <---- replace this text with that variable
-            showConfirmButton: false,
-            timer: 1500,
-            timerProgressBar: true,
-        }
-    );
+            title: 'Oops!',
+            text: 'Please enter a message to reverse'
+        });
+    } else {
+        // pass the input to reverseMessage() and assign its return value to a variable
+        let revMsg = reverseMessage(message);
+
+        // give the reverse message to displayessage()
+        displayMessage(revMsg);
+    }
 }
+
+// reverse the message
+function reverseMessage(input) { // if input = hello
+
+    let output = ''; // output = ['h', 'e', 'l', 'l', 'o']
+
+    for (let i = input.length-1; i >= 0; i--) {
+
+        output += input[i]
+    }
+
+    // turns ['o','l','l','e','h'] into ['olleh']
+    return output;
+}
+
+// display the reverse message
+function displayMessage(message) {
+
+    document.getElementById('msg').textContent = `Your message reversed is: ${message}`;
+
+    document.getElementById('alert').classList.remove('invisible');
+}
+
